6101479d676dffec568faf7b5086e10e92142db6
Hari Wicaksono <hariwicaksono87@gmail.com>
2020-10-28 15:57:22 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -381,9 +381,9 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver']             = 'database';
-$config['sess_cookie_name']        = 'ci_session';
+$config['sess_cookie_name']        = 'inv_session';
 $config['sess_expiration']         = 7200;
-$config['sess_save_path']          = 'ci_session';
+$config['sess_save_path']          = 'inv_session';
 $config['sess_match_ip']           = FALSE;
 $config['sess_time_to_update']     = 300;
 $config['sess_regenerate_destroy'] = FALSE;
